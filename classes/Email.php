c638bc310d8e3bdbf807819cc4d8ddc2181b5797
PotterFH <ivan_2333@outlook.com>
2023-09-18 15:49:53 -0600

    Avances

--- classes/Email.php
+++ classes/Email.php
@@ -39,7 +39,7 @@ class Email
         $contenido  = "<html>";
         $contenido .= "<p><strong> Hola " . $this->nombre . "</strong> Has creado tu cuenta en AppSalon, solo debes confirmarla presionando en el siguiente enlace</p>";
         $contenido .= "<p>Presiona aqui: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar cuenta</a></p>";
-        $contenido .= "<p>Si tu no solicitaste esta cuenta, peudes ignorar el mensaje</p>";
+        $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .= "</html>";
         $phpmailer->Body = $contenido;
 
